8d302fbd64ffe0cd4c1db6d7c79d8d2507942453
VincentTuan <vincentjava@yahoo.com.tw>
2024-04-25 20:53:58 +0800

    Add files via upload

---
+++ ActivityRecommend.java
@@ -0,0 +1,19 @@
+package day4;
+
+/**
+活動推薦
+輸入今日天氣（1:晴朗、2:多雲、3:陰天、4:下雨）：
+若 1:晴朗 -> 外出打球
+若 2:多雲 -> 外出散步
+若 3:陰天 -> 外出散步
+若 4:下雨 -> 在家寫程式
+
+請利用 switch-case 完成
+*/
+
+public class ActivityRecommend {
+	public static void main(String[] args) {
+		
+
+	}
+}
