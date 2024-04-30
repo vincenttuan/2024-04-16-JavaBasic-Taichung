1dc2364ef00b68a03a3927c515c456602094c38f
VincentTuan <vincentjava@yahoo.com.tw>
2024-04-30 18:43:05 +0800

    Add files via upload

---
+++ day5/WhileDemo3.java
@@ -0,0 +1,15 @@
+package day5;
+
+public class WhileDemo3 {
+
+	public static void main(String[] args) {
+		// 印出 5 次 "Java" 字樣
+		int count = 5;
+		while(count > 0) {
+			System.out.println("Java");
+			count--; // 每次減一
+		}
+
+	}
+
+}
